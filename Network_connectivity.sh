ef90e515ed4779ac4f6a61978c862a9c65049cfc
Srilatha Devi Kota <srilko27@in.ibm.com>
2025-03-28 14:16:25 +0530

    adding network code

---
+++ Network_connectivity.sh
@@ -0,0 +1,14 @@
+#!/bib/bash
+SERVER="44.201.102.206"
+LOGFILE="/var/log/network_connectivity.log"
+TIMEOUT=5
+while true
+do
+  if ping -c 1 -w $TIMEOUT $SERVER &> /dev/null
+  then
+    :
+  else
+  echo "$(date)- $SERVER is unreachable">> $LOGFILE
+  fi
+ sleep 60
+done   
